8bee03d696cabb02027cf7470f451c3fb63cbe1d
iamswet <isitswet@gmail.com>
2024-10-27 23:58:37 +0530

    code changes

---
+++ Backend/src/models/user.js
@@ -0,0 +1,102 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const userschema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 7,
+      validate(value) {
+        if (value.includes("password")) {
+          throw new Error("Cannot contain sub string 'password'");
+        }
+      },
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Not a valid Email");
+        }
+      },
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    avatar: {
+      type: Buffer,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//userschema.methods.userPublicProfile= function(){             toJSON is used to return what we want hence we decide what data to send. instead of setting userPublicProfile, we simply use to JSON
+
+userschema.methods.toJSON = async function () {
+  const user = this;
+  const userObject = user.toObject();
+  delete userObject.password;
+  delete userObject.tokens;
+  delete userObject.avatar;
+  return userObject;
+};
+
+userschema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, "finishbysunday");
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
+  return token;
+};
+
+userschema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("Invalid input");
+  }
+  const isMatch = bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Invalid input");
+  }
+
+  return user;
+};
+
+//converting text password to bcrypt hash password
+userschema.pre("save", async function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
+
+const User = mongoose.model("User", userschema);
+
+module.exports = User;
---
+++ Backend/src/routers/user.js
@@ -0,0 +1,190 @@
+const express = require("express");
+const User = require("../models/user");
+const auth = require("../middleware/auth");
+const sharp= require("sharp")
+const multer = require("multer");
+const router = new express.Router();
+
+//creating user
+router.post("/create-user", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user: user, token });
+  } catch (error) {
+    res.send(400).send(error);
+  }
+
+});
+
+router.post("/user/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.status(200).send({ user, token });
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+});
+
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
+});
+
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.post("/users/logoutall", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.get("/fetchall", async (req, res) => {
+  try {
+    const usersdata = await User.find();
+    res.send(usersdata);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+
+});
+
+/* 
+
+We are removing this so that 1 user can't change fetch the data of other users (navigating to others accounts)
+
+router.get("/fetch/:id", async (req, res) => {
+  try {
+    const userdata = await User.findById(req.params.id);
+    if (!userdata) {
+      return res.status(404).send();
+    }
+    res.send(userdata);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+  });
+*/
+
+/** */
+//update profile
+//router.patch("/update-user/:id", async (req, res) => {
+router.patch("/users/me", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid request" });
+  }
+
+  try {
+    updates.forEach((update) => {
+      req.user[update] = req.body[update];
+    });
+    await req.user.save();
+    res.send(req.user).status(200);
+  } catch (error) {
+    res.send(error).status(400);
+  }
+});
+
+//  router.delete("/delete-user/:id", async (req, res) => {           specific user should be able to delete only their ID
+//    const userdata = await User.findByIdAndDelete(req.params.id);   if we use the delete-users/:id
+/*
+    const userdata = await User.findByIdAndDelete(req.params.id);
+    if (!userdata) {
+      return res.status(404).send();
+    }
+ */
+
+const upload = multer({
+  // the destination is used to mention the folder location where the file would be saved
+  //  dest: "avatar",
+  //limits is used to mention the file size and other properties too
+  limits: {
+    fileSize: 10000000,
+  },
+
+  //the file filter is simply meant to validate the file and ensure if it is an image or anything else
+  fileFilter(req, file, cb) {
+    //if(!file.originalname.endsWith(".pdf"))
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new CustomError("Please upload an image ONLY"));
+    }
+
+    return cb(undefined, true);
+  },
+});
+
+router.post(
+  "/users/me/avatar",
+  auth,
+  upload.single("avatar"),
+  async (req, res) => {
+    const buffer=await sharp(req.file.buffer).resize({width:250,height:250}).png().toBuffer()
+    //req.user.avatar=req.file.buffer
+    req.user.avatar=buffer
+    await req.user.save()
+    res.send()
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
+router.delete("/users/me/avatar",auth,async(req,res)=>{
+  req.user.avatar=undefined
+  await req.user.save()
+  res.send()
+})
+
+router.get("/users/:id/avatar",async (req,res)=>{
+  try{
+    const user=await User.findById(req.params.id)
+    if(!user || !user.avatar){
+      throw new Error()
+    }
+    res.set('Content-Type','image/png')
+    res.set('Content-Disposition', 'inline')
+    res.send(user.avatar)
+
+  }catch(error){
+    res.status(400).send()
+  }
+
+})
+
+router.delete("/users/me", auth, async (req, res) => {
+  try {
+    const userdata = await User.findByIdAndDelete(req.user._id);
+    res.send(req.user);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+
+
+
+module.exports = router;
